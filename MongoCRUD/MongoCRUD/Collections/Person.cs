be735e6ada23b0e3530b233b40f1252e206eb017
Lair Júnior <lairjr@outlook.com>
2015-06-05 01:08:17 -0300

    Added more functionalities

---
+++ MongoCRUD/MongoCRUD/Collections/Person.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MongoDB.Bson;
+
+namespace MongoCRUD.Collections
+{
+    public class Person
+    {
+        public ObjectId Id { get; set; }
+        public string Name { get; set; }
+        public int   Age { get; set; }
+        public string Profession { get; set; }
+        //public IEnumerable<string> Colors { get; set; }
+        //public IEnumerable<Pet> Pets { get; set; }
+        //public BsonDocument ExtraElements { get; set; }
+    }
+
+    public class Pet
+    {
+        public string Name { get; set; }
+        public string Type { get; set; }
+    }
+}
