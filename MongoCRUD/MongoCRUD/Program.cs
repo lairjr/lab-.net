65b2a1fed546977740e32541c166aebe534855ff
Lair Júnior <lairjr@outlook.com>
2015-06-04 22:29:12 -0300

    Added mongo crud project

---
+++ MongoCRUD/MongoCRUD/Program.cs
@@ -0,0 +1,21 @@
+﻿using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MongoCRUD
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        static async Task MainAsync(string[] args)
+        {
+            var client = new MongoClient();
+        }
+    }
+}
